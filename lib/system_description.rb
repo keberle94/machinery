a266c5c1f1a497722a7e35eb4b7d0fc4acf0db19
Andre Duffeck <aduffeck@suse.de>
2014-09-22 14:27:49 +0200

    Add "SystemDescription#to_hash"
    "SystemDescription#to_hash" returns the system description data as a
    regular ruby hash, without the data model classes on top.

--- lib/system_description.rb
+++ lib/system_description.rb
@@ -120,7 +120,7 @@ class SystemDescription < Machinery::Object
     end
   end
 
-  def to_json
+  def to_hash
     meta = {}
     meta["format_version"] = self.format_version if self.format_version
 
@@ -130,8 +130,11 @@ class SystemDescription < Machinery::Object
 
     hash = as_json
     hash["meta"] = meta unless meta.empty?
+    hash
+  end
 
-    JSON.pretty_generate(hash)
+  def to_json
+    JSON.pretty_generate(to_hash)
   end
 
   def scopes
