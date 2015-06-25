2e9b232b09837dd03cac21f3a13f2a295522db82
Andre Duffeck <aduffeck@suse.de>
2015-06-25 10:08:24 +0200

    Add section to packages comparisons for same-but-different packages
    The output will look like this:
    
        Only in 'description1':
          * bash
    
        Only in 'description2':
          * zypper
    
        In both with different attributes ('description1' <> 'description2'):
          * kernel-desktop (version: 3.7.10 <> 3.7.11)
          * autofs (vendor: openSUSE <> Packman)
          * btrfsprogs (version: 3.16 <> 3.18, vendor: openSUSE <> Packman)

--- plugins/packages/packages_renderer.rb
+++ plugins/packages/packages_renderer.rb
@@ -26,6 +26,51 @@ class PackagesRenderer < Renderer
     end
   end
 
+  def do_render_comparison(description1, description2, description_common)
+    @changed_packages = description1.packages.map(&:name) & description2.packages.map(&:name)
+
+    render_comparison_only_in(description1)
+    render_comparison_only_in(description2)
+    render_comparison_changed(description1, description2)
+    render_comparison_common(description_common)
+  end
+
+  # In the comparison case we only want to show the package name, not all details like version,
+  # architecture etc.
+  def do_render_comparison_only_in
+    packages = @system_description.packages.reject { |package| @changed_packages.include?(package.name) }
+
+    list do
+      packages.each do |p|
+        item "#{p.name}"
+      end
+    end
+  end
+
+  def render_comparison_changed(description1, description2)
+    return if @changed_packages.empty?
+
+    packages = description1.packages.select { |package| @changed_packages.include?(package.name) }
+
+    puts "In both with different attributes ('#{description1.name}' <> '#{description2.name}'):"
+    indent do
+      list do
+        packages.each do |package|
+          other_package = description2.packages.find { |p| package.name == p.name}
+
+          changes = []
+          ["version", "vendor"].each do |attribute|
+            if package[attribute] != other_package[attribute]
+              changes << "#{attribute}: #{package[attribute]} <> #{other_package[attribute]}"
+            end
+          end
+
+          item "#{package.name} (#{changes.join(", ")})"
+        end
+      end
+    end
+  end
+
   def display_name
     "Packages"
   end
