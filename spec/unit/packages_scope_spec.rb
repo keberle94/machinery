f0eb2b080fa76c9cb0918fe269457ec27013c5e9
Andre Duffeck <aduffeck@suse.de>
2015-06-29 09:52:29 +0200

    Change PackagesScope#compare_with to consider changed packages
    Same packages which have different attributes in the two scopes
    be put into the "changed" object of the comparison result array.

--- spec/unit/packages_scope_spec.rb
+++ spec/unit/packages_scope_spec.rb
@@ -48,36 +48,40 @@ describe PackagesScope do
       @package1 = Package.new(name: "PACK1", version: "1.0")
       @package2 = Package.new(name: "PACK2", version: "2.1")
       @package3 = Package.new(name: "PACK3", version: "4.5.6")
+      @package4_1 = Package.new(name: "PACK4", version: "4.5.6")
+      @package4_2 = Package.new(name: "PACK4", version: "4.5.7")
 
       @packages_scope1 = PackagesScope.new
-      @packages_scope1 << @package1 << @package2
+      @packages_scope1 << @package1 << @package2 << @package4_1
 
       @packages_scope2 = PackagesScope.new
-      @packages_scope2 << @package1 << @package2
+      @packages_scope2 << @package1 << @package2 << @package4_1
 
       @packages_scope3 = PackagesScope.new
-      @packages_scope3 << @package1 << @package3
+      @packages_scope3 << @package1 << @package3 << @package4_2
     end
 
     it "equal objects" do
       expect(@packages_scope1 == @packages_scope2).to be(true)
       expect(@packages_scope1.eql?(@packages_scope2)).to be(true)
 
-      only_in_scope1, only_in_scope2, common = @packages_scope1.compare_with(@packages_scope2)
+      only_in_scope1, only_in_scope2, similar, common = @packages_scope1.compare_with(@packages_scope2)
 
       expect(only_in_scope1).to be(nil)
       expect(only_in_scope2).to be(nil)
+      expect(similar).to be(nil)
 
-      expect(common.size).to eq(2)
+      expect(common.size).to eq(3)
       expect(common[0]).to eq(@package1)
       expect(common[1]).to eq(@package2)
+      expect(common[2]).to eq(@package4_1)
     end
 
     it "unequal objects" do
       expect(@packages_scope1 == @packages_scope3).to be(false)
       expect(@packages_scope1.eql?(@packages_scope3)).to be(false)
 
-      only_in_scope1, only_in_scope2, common = @packages_scope1.compare_with(@packages_scope3)
+      only_in_scope1, only_in_scope2, similar, common = @packages_scope1.compare_with(@packages_scope3)
 
       expect(only_in_scope1.size).to eq(1)
       expect(only_in_scope1[0]).to eq(@package2)
@@ -87,6 +91,10 @@ describe PackagesScope do
 
       expect(common.size).to eq(1)
       expect(common[0]).to eq(@package1)
+
+      expect(similar.size).to eq(1)
+      expect(similar[0][0]).to eq(@package4_1)
+      expect(similar[0][1]).to eq(@package4_2)
     end
   end
 
